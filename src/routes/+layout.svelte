fcee9be7bdcaaf3a3e95319d5345650a62f2d73c
Nikita Zykov <zykov@introvert.bz>
2024-02-21 15:02:44 +0300

    First_upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import LeftPanel from './LeftPanel.svelte';
+	// import './styles.css';
+</script>
+
+<div class="app">
+	<!-- <Header /> -->
+
+	<main>
+        <LeftPanel/>
+		<slot />
+	</main>
+
+
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+
+	@media (min-width: 480px) {
+	
+	}
+</style>
---
+++ src/routes/LeftPanel.svelte
@@ -0,0 +1,15 @@
+<div id="l_panel">
+
+
+
+
+</div>
+
+<style>
+
+    #l_panel{
+        height: 100vh;
+        background: #1771bb;
+    }
+
+</style>
