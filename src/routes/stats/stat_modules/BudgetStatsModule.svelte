99911bd63d0c5eba126b98550d68dcd072559d98
Nikita Zykov <zykov@introvert.bz>
2024-04-02 16:01:15 +0300

    Other_stats_lines

--- src/routes/stats/stat_modules/BudgetStatsModule.svelte
+++ src/routes/stats/stat_modules/BudgetStatsModule.svelte
@@ -1,6 +1,46 @@
 <script>
     // your script goes here
-    let total_budget_percentage = 85;
+    let total_budget_current = 3000;
+    let total_budget_plan = 30000;
+    let total_budget_percentage = Math.floor(total_budget_current / (total_budget_plan/100));
+    let other_stats = [
+      {
+        'category_name':"Food",
+        'sum_plan':9000,
+        'sum_current':4500,
+        'bg_color':'red'
+      },
+      {
+        'category_name':"Others",
+        'sum_plan':12000,
+        'sum_current':5000,
+        'bg_color':'blue'
+      }
+    ]
+
+    import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+
+	
+
+    function others_stats_line(node,{duration,current_sum,plan_sum}){
+     
+
+        return {
+        duration,
+        css: (t) => {
+          // const progress = tweened(0, {
+          //   duration: 2000,
+          //   easing: cubicOut
+          // });
+          let percentage = -100+Math.floor(current_sum / (plan_sum/100))
+          
+          return 'left:-50%';
+        }
+      };
+    }
+
+  
 </script>
 
 
@@ -9,8 +49,8 @@
         <div id="main_stat_block">
 
 
-            <span id="total_current_sum">25000</span>
-            <span id="total_plan_sum">30000</span>
+            <span id="total_current_sum">{total_budget_current}</span>
+            <span id="total_plan_sum">{total_budget_plan}</span>
             
             <div id="total_progress" style="--total_value: {total_budget_percentage};">
                     
@@ -18,10 +58,17 @@
 
         </div>
 
-        <div id="others_stat_block"></div>
+        <div id="others_stat_block">
+          {#each other_stats as item}
 
+              <div class="other_stat">
+                <span class="other_stat_title">{item.category_name}</span>
+                <span class="other_stat_sum">{item.sum_current}/{item.sum_plan}</span>
+                <div class="other_progress" ><span class="other_progress_bar" in:others_stats_line={{duration:2000,current_sum:item.sum_current,plan_sum:item.sum_plan}} style=" background: {item.bg_color};"></span></div>
+              </div>
 
-
+            {/each}
+        </div>
 
     </div>
 
@@ -46,23 +93,25 @@
   initial-value: 0;
 }
     @keyframes total_progress {
-        to {
-            --total_progress_value: var(--total_value);
+          to {
+              --total_progress_value: var(--total_value);
+          }
         }
-        }
+
+  
 
     #budget_stats_container{
         width: 20rem;
         height: fit-content;
-        min-height: 25rem;
+        padding-bottom: 2rem;
+        min-height: 20rem;
         border-radius: 10px;
         background: var(--stats_card_bg_color);
     }
 
     #main_stat_block{
         width:100%;
-        height: 15rem;
-        border-bottom: 1px solid black;
+        height: 14rem;
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -119,121 +168,49 @@
     }
  
 
+    #others_stat_block{
+      z-index: 20;
+      position: relative;
+      
+      
+      
+      .other_stat{
+          display: flex;
+          flex-wrap: wrap;
+          width: 100%;
+          margin-top: 1rem;
+          padding: 0 1rem;
+          span{
+            width: 50%;
+            font-size: 16px;
 
+            &.other_stat_sum{
+              text-align: end;
+            }
+          }
 
+          .other_progress{
+            height: 5px;
+            width: 100%;
+            margin-top: .5rem;
+            background: #b6b6b6;
+            position: relative;
+            overflow: hidden;
+            .other_progress_bar{
+                position: absolute;
+                top: 0;
+                width: 100%;
+                height: 10px;
+              
+                
+            }
+          }
+      }
+    }
 
-    @property --eb-progress-value {
-  syntax: "<integer>";
-  inherits: false;
-  initial-value: 0;
-}
+  
 
-@keyframes eb-progress {
-  to {
-    --eb-progress-value: var(--value);
-  }
-}
-
-body {
-  font-family: -apple-system, system-ui, Helvetica, Arial, sans-serif;
-  margin: auto;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  flex-wrap: wrap;
-  background-color: #f2f2f2;
-  max-width: 800px;
-  height: 100vh;
-}
-
-.eb-progress-bar {
-  --size: 4.5rem;
-  --inner-bg: #f2f2f2;
-  --primary-color: var(--col);
-  --secondary-color: #dfe0e0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-shadow: 0 0px 2px rgb(0 0 0 / 40%), inset 0 0px 1px rgb(0 0 0 / 30%),
-    0 1px 2px rgb(0 0 0 / 60%), 0 4px 2px rgb(0 0 0 / 20%),
-    0 9px 4px rgb(0 0 0 / 10%), inset 0 2px 1px #ffffff8c;
-  width: var(--size);
-  height: var(--size);
-  font-size: calc(var(--size) / 5);
-  color: var(--primary-color);
-  background: 
-    conic-gradient(
-       var(--primary-color) calc(var(--eb-progress-value) * 1%),
-      var(--secondary-color) 0
-    );
-//   border-radius: 50%;
-
-  &-wrapper {
-    display: grid;
-    place-items: center;
-    gap: 0.5rem;
-    padding: 1rem;
-    transform: scale(1.5);
-  }
-
-  &,
-  &::before {
-    animation: eb-progress 2s 1 forwards;
-  }
-
-//   &::before {
-//     counter-reset: percentage var(--eb-progress-value);
-//     content: counter(percentage) "%";
-//     font-weight: 400;
-//     font-family: Roboto;
-//     font-size: 15px;
-//     color: #9e9e9e;
-
-//     background: #f5f6f7;
-//     background-image: -webkit-gradient(
-//       linear,
-//       right top,
-//       right bottom,
-//       color-stop(0%, #ffffff),
-//       color-stop(100%, #d3d1cb)
-//     );
-
-//     box-shadow: 1px 1px 3px rgb(0 0 0 / 15%);
-//     padding: 20px 15px;
-//     border-radius: 50%;
-//     z-index: 1;
-//     position: relative;
-//     border: 1px solid #d0d0d0;
-//   }
-
-  &-title {
-    text-align: center;
-    color: inherit;
-    letter-spacing: 2px;
-    line-height: 1.2;
-    text-transform: uppercase;
-    font-size: 11px;
-    font-weight: 400;
-    font-family: Roboto;
-  }
-
-  progress {
-    visibility: hidden;
-    width: 0;
-    height: 0;
-  }
-}
-
-h2 {
-  font-family: "integralcfmedium" !important;
-  margin-top: 6px;
-}
-
-@media screen and (min-width: 768px) {
-  h1 {
-    font-size: 64px;
-  }
-}
+  
 
 </style>
 
